eb14c8d3b5806fd8c4811306559f627dcbed8005
Aboubkr <aboubkrmohamed86@gmail.com>
2023-10-24 18:58:20 +0300

    f

--- js/loading.js
+++ js/loading.js
@@ -4,7 +4,7 @@ const section =document.getElementById('section');
 function myFunction() {
 setTimeout(()=>{
 document.getElementById("loading").style.display = "none";
-document.getElementById("section").style.height = "auto";
+// document.getElementById("section").style.height = "auto";
 
 
 }, 3000);
